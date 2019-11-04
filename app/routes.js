fab4ed6569fdee03b85c04a4f26dc3cacacdce05
leah smith <leahsmith1104@gmail.com>
2019-11-04 15:56:38 +0000

    Update routes.js

--- app/routes.js
+++ app/routes.js
@@ -49,21 +49,9 @@ router.post('v2/dc-commitments-level-1', function (req, res) {
 // Notify routes
 
 router.post('/v2/status', function (req, res) {
-
-console.log('req.session.data[status]', req.session.data);
-  
-  
   // let status = req.body.statusAccepted;
   let status = req.session.data['status']
   if (status === 'rejected') {
-    notify.sendEmail(
-      // this long string is the template ID, copy it from the template
-      // page in GOV.UK Notify. It’s not a secret so it’s fine to put it
-      // in your code.
-      '9d037de7-f79c-42f1-9c67-2535dce0d170',
-      // pulls 'emailAddress' variable set in the config file
-      config.emailAddress
-    );
     res.redirect('/v2/status-reason-for-rejection')
   }
   else {
@@ -78,5 +66,55 @@ console.log('req.session.data[status]', req.session.data);
     res.redirect('/v2/status-accepted')
   }
 });
+
+router.post('/v2/status-reason-for-rejection', function (req, res) {
+    // let status = req.body.statusAccepted;
+    let status = req.session.data['status']
+    if (status === 'not-UK') {
+      notify.sendEmail(
+        // this long string is the template ID, copy it from the template
+        // page in GOV.UK Notify. It’s not a secret so it’s fine to put it
+        // in your code.
+        'baa99093-c0f5-4624-8615-67e6290c1e7b',
+        // pulls 'emailAddress' variable set in the config file
+        config.emailAddress
+      );
+      res.redirect('/v2/status-not-uk')
+    }
+    if (status === 'duplicate') {
+      notify.sendEmail(
+        // this long string is the template ID, copy it from the template
+        // page in GOV.UK Notify. It’s not a secret so it’s fine to put it
+        // in your code.
+        'e40ecf01-d563-45a7-b31d-38087d330803',
+        // pulls 'emailAddress' variable set in the config file
+        config.emailAddress
+      );
+      res.redirect('/v2/status-duplicate')
+    }
+
+    if (status === 'already-national') {
+      notify.sendEmail(
+        // this long string is the template ID, copy it from the template
+        // page in GOV.UK Notify. It’s not a secret so it’s fine to put it
+        // in your code.
+        '27c9b1df-1a55-44b1-bd7b-7001709d16dc',
+        // pulls 'emailAddress' variable set in the config file
+        config.emailAddress
+      );
+      res.redirect('/v2/status-already-national')
+    }
+    else {
+      notify.sendEmail(
+        // this long string is the template ID, copy it from the template
+        // page in GOV.UK Notify. It’s not a secret so it’s fine to put it
+        // in your code.
+        '9d037de7-f79c-42f1-9c67-2535dce0d170',
+        // pulls 'emailAddress' variable set in the config file
+        config.emailAddress
+      );
+      res.redirect('/v2/status-cannot-verify')
+    }
+  });
 
 module.exports = router
